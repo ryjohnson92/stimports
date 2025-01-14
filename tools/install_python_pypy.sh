52645c4cdf6fddfacc447d5d03d9d738dc07ff9a
Ryan <ryan.johnson@gtl.net>
2025-01-14 12:12:58 -0500

    adding full build pypy

---
+++ tools/install_python_pypy.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+echo "expanding pypy directory";
+tar xvf /tmp/stimports/applications/python/pypy3.10.tar.xz -C /tmp/python;
+mkdir /etc/pypy3.10;
+cp -r /tmp/python/pypy3.10/* /etc/pypy3.10/;
+ln -s /etc/pypy3.10/bin/pypy3.10 /usr/local/bin/python;
+chmod +x /usr/local/bin/python;
+python -m ensurepip;
