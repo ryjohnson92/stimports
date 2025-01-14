301e397d3427b1ae8d6a49a65ca70968a90dcfc6
Ryan <ryan.johnson@gtl.net>
2025-01-14 15:10:25 -0500

    resolved git clone dir

--- tools/install_python_modules.sh
+++ tools/install_python_modules.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-cd $PYTHON_MODULE_DIR
-git clone https://github.com/ryjohnson92/Containers.git && \
-    git clone https://github.com/ryjohnson92/Remote.git && \
-    git clone https://github.com/ryjohnson92/Local.git
+cd $PYTHON_MODULE_DIR;
+git -C $PYTHON_MODULE_DIR clone https://github.com/ryjohnson92/Containers.git && \
+    git -C $PYTHON_MODULE_DIR clone https://github.com/ryjohnson92/Remote.git && \
+    git -C $PYTHON_MODULE_DIR clone https://github.com/ryjohnson92/Local.git;
