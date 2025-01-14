e007185fce74b52afc5426781dd160a630f25aa8
Ryan <ryan.johnson@gtl.net>
2025-01-14 12:25:14 -0500

    py313 install

---
+++ tools/install_python_313.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+echo "expanding pypy directory";
+tar xvf /tmp/stimports/applications/python/Python-3.13.0a5.tar.xz -C /tmp/python;
+cd /tmp/python/Python-3.13.0a5
+./configure --enable-optimizations --disable-gil --enable-experimental-jit && \
+    make -j4 && \
+    make install;
+ln -s /usr/local/bin/python3.13t /usr/local/bin/python;
