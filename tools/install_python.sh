188d01132f35f7a491650ce929f31dcd95954039
Ryan <ryan.johnson@gtl.net>
2025-01-14 15:27:12 -0500

    One more time

--- tools/install_python.sh
+++ tools/install_python.sh
@@ -11,9 +11,6 @@ else
     bash /tmp/stimports/tools/install_$PYTHON_VERSION.sh
 fi
 # fi
-mkdir mkdir -p /var/lib/python;
-ln -s $PYTHON_MODULE_DIR /var/lib/python/modules;
-
 bash /tmp/stimports/tools/install_python_modules.sh
 ## Need this on your install script 
 # echo 'PYTHON_MODULE_DIR="/etc/pypy3.10/lib/pypy3.10/site-packages"' >>   ~/.bashrc
--- tools/install_python_modules.sh
+++ tools/install_python_modules.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
-cd $PYTHON_MODULE_DIR;
+mkdir mkdir -p /var/lib/python;
+ln -s $PYTHON_MODULE_DIR /var/lib/python/modules;
 git -C $PYTHON_MODULE_DIR clone https://github.com/ryjohnson92/Containers.git && \
     git -C $PYTHON_MODULE_DIR clone https://github.com/ryjohnson92/Remote.git && \
     git -C $PYTHON_MODULE_DIR clone https://github.com/ryjohnson92/Local.git;
