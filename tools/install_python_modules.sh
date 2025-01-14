34e536cdf373dcf0b9a078bf66e5d737a5c780df
Ryan <ryan.johnson@gtl.net>
2025-01-14 15:32:54 -0500

    oops.html

--- tools/install_python_modules.sh
+++ tools/install_python_modules.sh
@@ -1,6 +1,3 @@
-#! /usr/bin/bash
-mkdir mkdir -p /var/lib/python;
-ln -s $PYTHON_MODULE_DIR /var/lib/python/modules;
-git -C $PYTHON_MODULE_DIR clone https://github.com/ryjohnson92/Containers.git && \
-    git -C $PYTHON_MODULE_DIR clone https://github.com/ryjohnson92/Remote.git && \
-    git -C $PYTHON_MODULE_DIR clone https://github.com/ryjohnson92/Local.git;
+cd $PYTHON_MODULE_DIR && git clone https://github.com/ryjohnson92/Containers.git && \
+    git clone https://github.com/ryjohnson92/Remote.git && \
+    git clone https://github.com/ryjohnson92/Local.git;
