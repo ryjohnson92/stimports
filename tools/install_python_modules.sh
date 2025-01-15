d428c70d58d1c0ef3fe764c8c61a036c19d5ad13
Ryan <ryan.johnson@gtl.net>
2025-01-15 10:30:10 -0500

    adding web

--- tools/install_python_modules.sh
+++ tools/install_python_modules.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/ryjohnson92/Containers.git /var/lib/python/modules/Containers && \
     git clone https://github.com/ryjohnson92/Remote.git /var/lib/python/modules/Remote && \
-    git clone https://github.com/ryjohnson92/Local.git /var/lib/python/modules/Local;
+    git clone https://github.com/ryjohnson92/Local.git /var/lib/python/modules/Local && \
+    git clone https://github.com/ryjohnson92/Web.git /var/lib/python/modules/Local;
