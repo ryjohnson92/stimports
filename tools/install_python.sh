dd113b39c00f95d44b8330400e7029fa973b0f71
Ryan <ryan.johnson@gtl.net>
2025-01-15 13:40:48 -0500

    changed ordering

--- tools/install_python.sh
+++ tools/install_python.sh
@@ -11,8 +11,9 @@ else
     bash /tmp/stimports/tools/install_$PYTHON_VERSION.sh
 fi
 # fi
-bash /tmp/stimports/tools/install_python_modules.sh
 python -m pip install -r /tmp/stimports/requirements.txt
+bash /tmp/stimports/tools/install_python_modules.sh
+
 ## Need this on your install script 
 # ln -s /usr/local/lib/python3.13/site-packages /var/lib/python/modules;
 
