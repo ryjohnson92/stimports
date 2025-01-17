216cb46fd832fc227708f25a2ce0a877b9d918b1
Ryan <ryan.johnson@gtl.net>
2025-01-17 11:22:06 -0500

    adding PyTools module

--- tools/install_python_modules.sh
+++ tools/install_python_modules.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/ryjohnson92/Containers.git /var/lib/python/modules/Containers && \
     git clone https://github.com/ryjohnson92/Remote.git /var/lib/python/modules/Remote && \
     git clone https://github.com/ryjohnson92/Local.git /var/lib/python/modules/Local && \
-    git clone https://github.com/ryjohnson92/Web.git /var/lib/python/modules/Web;
+    git clone https://github.com/ryjohnson92/Web.git /var/lib/python/modules/Web && \
+    git clone https://github.com/ryjohnson92/pytools.git /var/lib/python/modules/PyTools;
 
 
