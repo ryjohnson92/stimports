7593a5abd05257bb750c997006d323bfb553235b
ryjohnson <ryjohnson@localhost.localdomain>
2025-09-17 09:50:48 -0400

    resolve pypy311 issues

--- tools/install_python_pypy311.sh
+++ tools/install_python_pypy311.sh
@@ -2,7 +2,7 @@
 echo "expanding pypy directory";
 tar xjf /tmp/stimports/applications/python/pypy3.11-v7.3.20-linux64.tar.bz2 -C /tmp/python;
 mkdir /etc/pypy3.11;
-cp -r /tmp/python/pypy3.11/* /etc/pypy3.11/;
+cp -r /tmp/python/pypy3.11-v7.3.20-linux64/* /etc/pypy3.11/;
 ln -s /etc/pypy3.11/bin/pypy3.11 /usr/local/bin/python;
 chmod +x /usr/local/bin/python;
 python -m ensurepip;
